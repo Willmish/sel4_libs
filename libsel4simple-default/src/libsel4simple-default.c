b8e98a32ab92affff7b3bfe5277c415462824ca5
Sam Leffler <sleffler@google.com>
2022-09-27 20:35:59 +0000

    seL4_libs: skip tainted untypeds during bootstrap
    Untyped slabs are marked tainted when the kerrnel uses them to back
    rootserver resources. They should not be used to bootstrap dynamic
    allocators because the current logic assumes untypeds provided by
    the kernel are empty/unused (so mucking with them will corrupt the
    rootserver's VSpace).
    
    Change-Id: I1912131c1cc066224120f73d6abf9daed6b2ced7

--- libsel4simple-default/src/libsel4simple-default.c
+++ libsel4simple-default/src/libsel4simple-default.c
@@ -139,7 +139,7 @@ int simple_default_untyped_count(void *data)
     return ((seL4_BootInfo *)data)->untyped.end - ((seL4_BootInfo *)data)->untyped.start;
 }
 
-seL4_CPtr simple_default_nth_untyped(void *data, int n, size_t *size_bits, uintptr_t *paddr, bool *device)
+seL4_CPtr simple_default_nth_untyped(void *data, int n, size_t *size_bits, uintptr_t *paddr, bool *device, bool *isTainted)
 {
     assert(data);
 
@@ -154,6 +154,9 @@ seL4_CPtr simple_default_nth_untyped(void *data, int n, size_t *size_bits, uintp
         }
         if (device != NULL) {
             *device = (bool)bi->untypedList[n].isDevice;
+        }
+        if (isTainted != NULL) {
+            *isTainted = (bool)bi->untypedList[n].isTainted;
         }
         return bi->untyped.start + (n);
     }
